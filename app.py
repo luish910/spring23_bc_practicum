a5f9ecac75ecb1c6a05be34dfbf494123cdcef63
Luis Murrugarra <66395875+luish910@users.noreply.github.com>
2023-04-04 20:11:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ import os
 
 #files
 directory = os.getcwd()
-model_output_path = os.path.join(directory, "model-output")
-paintings_csv_name = 'WikiArtFull_2colors.csv'
+model_output_path = os.path.join(directory, "api/model-output")
+paintings_csv_name = 'WikiArtFull_colors.csv'
 movements_csv_name = 'WikiArtFull_Region_Mov_Year.csv'
 paintings_path = os.path.join(model_output_path, paintings_csv_name)
 movements_path = os.path.join(model_output_path, movements_csv_name)
 
 app = Flask(__name__)
-
 
 df_paintings = pd.read_csv(paintings_path)
 df_paintings.columns = ['id', 'year', 'year_group', 'artist', 'title', 'style', 'movement',
@@ -63,6 +62,8 @@ def region():
     df_response = pd.DataFrame(df_response['region'].value_counts().reset_index())
     df_response.columns = ['region','count']
     response_len = len(df_response)
+    region_palette= ['#a4d3e6','#f5e5c0','#9bc995','#f2b482','#b9887d']
+    df_response['fillColor'] = region_palette[0:region_palette]
     try:
         if response_len == 0:
             return '', 204
@@ -73,10 +74,6 @@ def region():
     except:
         abort(404)
 
-@app.route("/regiontest")
-def regiontest():
-    response = jsonify(df_paintings.to_dict(orient = "records"))
-    return response
    
 @app.route("/paintings")
 def paintings():
@@ -88,8 +85,7 @@ def paintings():
 
     year_from = int(year_from)
     year_to = int(year_to)
-
-    #movement = 'Minimalism'
+    
     cond_1 = df_paintings['region'] == region
     cond_2 = df_paintings['year'] >= year_from
     cond_3 = df_paintings['year'] <= year_to
